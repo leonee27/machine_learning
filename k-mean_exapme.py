a489026c03dd2c33c6bb7435f824e51257403774
leonee27 <leonee3721@gmail.com>
2018-09-08 13:42:55 -0400

    commit changes

--- k-mean_exapme.py
+++ k-mean_exapme.py
@@ -1,2 +1,19 @@
-%matplotlib inline
+import matplotlib
 import matplotlib.pyplot as plt
+import seaborn as sns; sns.set()
+import numpy as np
+
+from sklearn.datasets.samples_generator import make_blobs
+
+X, y_true = make_blobs(n_samples=300, centers=4, cluster_std=0.60, random_state=0)
+plt.scatter(X[:, 0], X[:, 1], s=50)
+plt.show()
+
+from sklearn.cluster import KMeans
+kmeans = KMeans(n_clusters=4)
+kmeans.fit(X)
+y_kmeans = kmeans.predict(X)
+
+centers = kmeans.cluster_centers_
+plt.scatter(centers[:,0], centers[:,1],c='black')
+plt.show()
