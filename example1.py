9447c9eaf9d1f1aefc6b030662d168c6aff3e130
leonee27 <leonee3721@gmail.com>
2018-09-24 22:25:00 -0400

    commit

--- example1.py
+++ example1.py
@@ -23,7 +23,75 @@ def lengthOfLongestSubstring(s):
     # for i in range(1, len(s)):
 
 
-    print(sub)
-    print(s[::3])
+    #print(sub)
+    #print(s[::3])
 
 lengthOfLongestSubstring(s)
+
+people = ['Dr. Christopher Brooks', 'Dr. Kevyn Collins-Thompson', 'Dr. VG Vinod Vydiswaran', 'Dr. Daniel Romero']
+
+
+for person in people:
+    #print(person.split()[0])
+    print(person.split()[0]+ '__' +person.split()[-1])
+
+add = lambda x,y,z: x+y
+print(add(3,5,6))
+
+
+
+#option 1
+#for person in people:
+#    print(split_title_and_name(person) == (lambda x: x.split()[0] + ' ' + x.split()[-1])(person))
+
+#option 2
+# list(map(split_title_and_name, people)) == list(map(lambda person: person.split()[0] + ' ' + person.split()[-1], people))
+
+
+my_list = []
+for number in range(0,1000):
+    if number % 2 == 0:
+        my_list.append(number)
+
+#print(my_list)
+
+my_list = [number for number in range(0,1000) if number % 2 == 0]
+print(my_list)
+print(people[1:])
+
+import numpy as np
+
+mylist = [1,2,3]
+x = np.array(mylist)
+y = np.array([7,8,9])
+
+m = np.array([[7,8,9], [10,11,12]])
+print(m)
+print(m.shape)
+
+n = np.arange(0,30,2)
+print(n.shape)
+
+n1 = n.reshape(3,5)
+n2 = n.resize(3,5)
+
+print(n1)
+print(n.resize(3,5))
+
+r = np.arange(36)
+r.resize((6,6))
+print(r)
+
+import pandas as pd
+
+s = pd.Series(np.random.randint(0,5,20))
+print(s)
+
+import timeit
+
+print timeit.timeit(setup= 'from math import sqrt', stmt = mycode, number = 10000)
+
+
+
+
+
